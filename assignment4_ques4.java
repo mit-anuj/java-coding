0534f83015284ae82322e6186f710dbaab4e742a
mit-anuj <anujmittal0040@gmail.com>
2023-01-27 23:28:21 +0530

    technical training

--- assignment4_ques4.java
+++ assignment4_ques4.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class assignment4_ques4 {
     public static void main(String[] args) {
-        
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter number: ");
+        int num=sc.nextInt();
+        int dupnum=num,duplicate=num;
+        int countDigit=0;
+        while(dupnum!=0)
+        {
+            dupnum/=10;
+            countDigit++;
+        }
+        int sum=0;
+        int digit;
+        while(num!=0){
+            digit=num%10;
+            sum+=Math.pow( digit,countDigit);
+            num/=10;
+        }
+
+        if(sum==duplicate)
+            System.out.println("number is armstrong");
+        else
+            System.out.println("not");
+
+
+        sc.close();
     }
 }
